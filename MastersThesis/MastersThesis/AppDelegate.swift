22e0dc23200764f9253057f4375c48a0c9c96836
bozidar <bozidar.labas@hotmail.com>
2016-09-01 10:42:01 +0200

    Initial commit

---
+++ MastersThesis/MastersThesis/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MastersThesis
+//
+//  Created by Bozidar on 01.09.2016..
+//  Copyright © 2016. Bozidar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
